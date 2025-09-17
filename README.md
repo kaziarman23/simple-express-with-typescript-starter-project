dd7122603c10ee2c66f9f15e861a96747a5ad6ae
kaziarman23 <kaziarman@proton.me>
2025-09-17 19:24:06 +0600

    update readme file

--- README.md
+++ README.md
@@ -73,7 +73,7 @@ Follow these steps to get the project running on your local machine.
 1.  **Clone the repository:**
     ```bash
     git clone https://github.com/kaziarman23/simple-express-with-typescript-starter-project.git
-    cd clone-project
+    cd simple-express-with-typescript-starter-project
     ```
 
 2.  **Install dependencies:**
--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,10 @@ import config from "./app/configs";
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log("Connected to MongoDB successfully! 🍃");
+    console.log("Connected to MongoDB successfully!");
 
     app.listen(config.port, () => {
-      console.log(`Book Store app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
